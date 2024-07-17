cfd8e1e3e21915c85371a053a3eed281f11fd15a
gaetanovenuto <gaetanovenuto99@live.it>
2024-07-17 12:48:19 +0200

    Creata struttura, iniziato JS

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+const items = document.getElementById('items');
+
+items.innerHTML += `
+<div class="item active">
+    <img src="img/01.webp">
+</div>
+<div class="item">
+    <img src="img/02.webp">
+</div>
+<div class="item">
+    <img src="img/03.webp">
+</div>
+<div class="item">
+    <img src="img/04.webp">
+</div>
+<div class="item">
+    <img src="img/05.webp">
+</div>
+`
+
+const singleItem = document.getElementsByClassName('item');
+
+let activeItem = 0;
+
+const imageCarousel = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+]
+
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+
+nextButton.addEventListener('click',
+    function() {
+        if (activeItem < singleItem.length - 1) {
+            item.classList.remove('active');
+
+            activeItem++;
+
+            item[activeItem].classList.add('active');
+        }
+    }
+)
+
+prevButton.addEventListener('click',
+    function() {
+        if (activeItem > 0) {
+            item.classList.remove('active');
+
+            imageCarousel++;
+
+            item[imageCarousel].classList.add('active');
+        }
+    }
+)
