44311908ad096ba9c15a9f1480ca271ff70dffa0
gaetanovenuto <gaetanovenuto99@live.it>
2024-07-18 10:48:56 +0200

    Completato bonus 2, aggiunte immagini al bonus 2 on input

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,46 @@
 const items = document.getElementById('items');
 
 items.innerHTML += `
-<div class="item active">
+<div class="item col-10 active">
     <img src="img/01.webp">
 </div>
-<div class="item">
+<div class="item col-10">
     <img src="img/02.webp">
 </div>
-<div class="item">
+<div class="item col-10">
     <img src="img/03.webp">
 </div>
-<div class="item">
+<div class="item col-10">
     <img src="img/04.webp">
 </div>
-<div class="item">
+<div class="item col-10">
     <img src="img/05.webp">
 </div>
 `
 
 const singleItem = document.getElementsByClassName('item');
+
+const thumbContainer = document.getElementById('thumbnail-images');
+thumbContainer.innerHTML += `
+<div class="thumb-img opacity-1">
+    <img src="img/01.webp">                    
+</div>
+<div class="thumb-img">
+    <img src="img/02.webp">
+</div>
+<div class="thumb-img">
+    <img src="img/03.webp">
+</div>
+<div class="thumb-img">
+    <img src="img/04.webp">
+</div>
+<div class="thumb-img">
+    <img src="img/05.webp">
+</div>
+`
+
+const thumbImg = document.getElementsByClassName('thumb-img');
+
 
 let activeItem = 0;
 
@@ -37,18 +59,22 @@ nextButton.addEventListener('click',
     function() {
         if (activeItem < imageCarousel.length - 1) {
             singleItem[activeItem].classList.remove('active');
+            thumbImg[activeItem].classList.remove('opacity-1');
 
             activeItem++;
 
             singleItem[activeItem].classList.add('active');
+            thumbImg[activeItem].classList.add('opacity-1');
         }
 
         else if (activeItem == imageCarousel.length - 1) {
             singleItem[activeItem].classList.remove('active');
+            thumbImg[activeItem].classList.remove('opacity-1');
 
             activeItem = 0;
 
             singleItem[activeItem].classList.add('active');
+            thumbImg[activeItem].classList.add('opacity-1');
         }
     }
 )
@@ -57,18 +83,22 @@ prevButton.addEventListener('click',
     function() {
         if (activeItem > 0) {
             singleItem[activeItem].classList.remove('active');
+            thumbImg[activeItem].classList.remove('opacity-1');
 
             activeItem--;
 
             singleItem[activeItem].classList.add('active');
-            }
+            thumbImg[activeItem].classList.add('opacity-1');
+        }
 
         else if (activeItem == 0) {
             singleItem[activeItem].classList.remove('active');
+            thumbImg[activeItem].classList.remove('opacity-1');
 
             activeItem = imageCarousel.length - 1;
 
             singleItem[activeItem].classList.add('active');
+            thumbImg[activeItem].classList.add('opacity-1');
         }
         }
 )
@@ -88,9 +118,14 @@ imageButton.addEventListener('submit',
     console.log(imageCarousel);
 
     items.innerHTML += `
-    <div class="item">
-        <img src="${imageURL}">
-    </div>
+        <div class="item">
+            <img src="${imageURL}">
+        </div>
+    `
+    thumbContainer.innerHTML += `
+        <div class="thumb-img">
+            <img src="${imageURL}">                    
+        </div>
     `
 
     console.log(items)
